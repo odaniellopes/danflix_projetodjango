cf5b1c38648d8deac33cc2a97df5bf85ac9c0bb9
Daniel Lopes <danilc344@gmail.com>
2022-12-15 10:46:00 -0300

    adicionar bd postgres

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -86,6 +86,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600,
+    conn_health_checks=True,
+)
 
 
 # Password validation
