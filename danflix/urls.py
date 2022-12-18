02d7ee91810dabeb0b076014a97dee4f3fd9f476
odaniellopes <danilc344@gmail.com>
2022-12-18 19:09:34 -0300

    correção do danflix

--- danflix/urls.py
+++ danflix/urls.py
@@ -1,4 +1,4 @@
-"""hashflix URL Configuration
+"""danflix URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
